656681c41c590e5a670db4706b3eedc303d05bdf
kintan <kingslay@icloud.com>
2026-01-30 01:04:08 +0800

    update

--- main.swift
+++ main.swift
@@ -7,16 +7,16 @@ func createM3u() throws {
     let url = URL(filePath: path)
     let dirName = url.lastPathComponent
     let filenames = try FileManager.default.contentsOfDirectory(atPath: path)
-    
+
     var data = "#EXTM3U"
-    filenames.filter{ filename in
+    filenames.filter { filename in
         if let typeID = try? url.appendingPathComponent(filename).resourceValues(forKeys: [.typeIdentifierKey]).typeIdentifier as CFString? {
             return UTTypeConformsTo(typeID, kUTTypeMovie) || UTTypeConformsTo(typeID, kUTTypeAudio)
         }
         return false
     }.forEach { filename in
         data += """
-        
+
         #EXTINF:-1 group-title="github",\(filename)
         ./\(filename)
         """
